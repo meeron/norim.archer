26b1174c00e12ec812ead1506ffdfdccad661d12
Miron Jakubowski <mijakubowski@gmail.com>
2018-04-04 22:56:25 +0200

    Basic application structure

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Navbar />
+    <div class="container">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: { Navbar }
+}
+</script>
+
 
 <style lang="scss">
 </style>
---
+++ web/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <ul class="right">
+        <li v-for="r in routes"
+          :key="r.path"
+          :class="{ active: $route.path === r.path }">
+          <router-link :to="r.path">{{r.title}} {{r.isActive}}</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      routes: [
+        { path: '/', title: 'Home' },
+        { path: '/login', title: 'Login' },
+        { path: '/about', title: 'About' }
+      ]
+    };
+  }
+};
+</script>
+
